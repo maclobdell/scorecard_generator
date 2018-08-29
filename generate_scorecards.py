078d314cac175ec2d53a6d25ffbf921024cf17b5
maclobdell <maclain.lobdell@arm.com>
2018-08-29 16:18:37 -0500

    clean up comments.  Add summarization of test results.

--- generate_scorecards.py
+++ generate_scorecards.py
@@ -11,27 +11,19 @@ from json import load, dump
 import re
 import logging
 
-#this script must run in a mbed-os top level directory
-ROOT = "."
-sys.path.insert(0, ROOT)
-from tools.targets import TARGETS
-
 parser = argparse.ArgumentParser(description='Generate Scorecards')
 parser.add_argument("-d", "--dontrun", default=0, action='count', help="print commands, don't run them")  #just check if present
 parser.add_argument("-f", "--folder", default="test_output", action='store', help="Folder to find test results")  #default is string
 
 def main():
 #  1. check that all three log files are present, if so, generate the scorecard_data
-#       a. get all possible "device_has" data for any targets, use that as the template for all targets,
-#          then get "device_has" data for the target, add to scorecard
-#        b. read each test log file and add data to the scorecard
-#             print any errors
-#  2.  validate scorecard and report any errors (create an error.txt file to log errors for each target)
+#        Read each test log file and add data to the scorecard
+#  2.  validate scorecard and report any errors
     
     args = parser.parse_args()
     folder = args.folder
 
-# files go in folder structure /<test_output>/<mbed_version>/<platform>
+    #files are in folder structure /<test_output>/<mbed_version>/<platform>
 
     #change to the test results directory
     try:
@@ -142,13 +134,29 @@ def generate_scorecard(folder_path, target, mbed_ver, log) :
             scorecard_data["device_has"] = target_data["device_has"]
 
 
-    #TODO - analyze the scorecard data,         
+    test_dictionary = {"ethernet": [], "wifi": [" tests-network-wifi\n"], "rtc": [" tests-mbed_drivers-rtc\n", " tests-mbed_hal-rtc\n", " tests-mbed_hal-rtc_reset\n", " tests-mbed_hal-rtc_time\n", " tests-mbed_hal-rtc_time_conv\n"], "uart": [], "flash": [" features-tests-filesystem-flashsim_block_device\n", " tests-mbed_drivers-flashiap\n", " tests-mbed_hal-flash\n"], "rtos": [" tests-mbedmicro-rtos-mbed-basic\n", " tests-mbedmicro-rtos-mbed-circularbuffer\n",   "tests-mbedmicro-rtos-mbed-condition_variable\n", " tests-mbedmicro-rtos-mbed-event_flags\n", " tests-mbedmicro-rtos-mbed-heap_and_stack\n", " tests-mbedmicro-rtos-mbed-kernel_tick_count\n", " tests-mbedmicro-rtos-mbed-mail\n", " tests-mbedmicro-rtos-mbed-malloc\n", " tests-mbedmicro-rtos-mbed-memorypool\n", " tests-mbedmicro-rtos-mbed-mutex\n", " tests-mbedmicro-rtos-mbed-queue\n", " tests-mbedmicro-rtos-mbed-rtostimer\n", " tests-mbedmicro-rtos-mbed-semaphore\n", " tests-mbedmicro-rtos-mbed-signals\n", " tests-mbedmicro-rtos-mbed-systimer\n", " tests-mbedmicro-rtos-mbed-threads\n"], "6lowpan": [], "timer": [" tests-mbed_drivers-lp_timer\n", " tests-mbed_drivers-timer\n", " tests-mbed_drivers-timerevent\n", " tests-mbedmicro-rtos-mbed-rtostimer\n", " tests-mbedmicro-rtos-mbed-systimer\n"], "trng": [], "spi": [], "emac": [" tests-network-emac\n"], "sleep": [" tests-mbed_drivers-sleep_lock\n", " tests-mbed_hal-sleep\n", " tests-mbed_hal-sleep_manager\n", " tests-mbed_hal-sleep_manager_racecondition\n"], "cellular": [" features-cellular-tests-api-cellular_device\n", " features-cellular-tests-api-cellular_information\n", " features-cellular-tests-api-cellular_network\n", " features-cellular-tests-api-cellular_power\n", " features-cellular-tests-api-cellular_sim\n", " features-cellular-tests-socket-udp\n", " features-netsocket-cellular-generic_modem_driver-tests-unit_tests-default\n"], "digital": [], "ble": [" tests-mbedmicro-rtos-mbed-condition_variable\n"], "watchdog": [], "i2c": [], "ticker": [" tests-mbed_drivers-lp_ticker\n", " tests-mbed_drivers-ticker\n", " tests-mbed_hal-common_tickers\n", " tests-mbed_hal-common_tickers_freq\n", " tests-mbed_hal-lp_ticker\n", " tests-mbed_hal-ticker\n", " tests-mbed_hal-us_ticker\n"], "lorawan": [" tests-lorawan-loraradio\n"], "analog": []}
+
+    requirement_results = {}
+    for keyword in test_dictionary:  #keyword is like the requirement category
+        for required_test in test_dictionary[keyword]:  #for each test that supports a category
+            #print keyword + " " + tests
+                #check the test results for the three compilers
     
-    #right now you have a dictionary of test suites and their result
     
-    #for each requirement, find each test that satisfies the requirement
-    
-    #Mbed_Enabled[Requirement_ID] = [test_suite_name, test_suite_name2, test_suite_name3, ...]
+            for toolchain in ["ARM", "GCC_ARM", "IAR"]:      
+                test_results = scorecard_data[toolchain]
+                for test_suite_name in test_results:  #get test name
+                    test_suite_result = test_results[test_suite_name]  #get test result
+
+                    if test_suite_name == required_test:
+                        #check result
+                        if test_suite_result == 'OK':              #add this result to the tally          
+                            requirement_results[keyword] = 'OK'
+                        else:
+                            requirement_results[keyword] = 'FAIL'  
+                        
+    print requirement_results
+    scorecard_data['Mbed_Enabled_Requirements'] = requirement_results
 
     #TODO - put all scorecard files in the same folder, not down in the numerous folders
         
